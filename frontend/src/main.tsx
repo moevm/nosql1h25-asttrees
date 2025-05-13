48484bc11397f4a79a43c7f61c4dd20690d4a9be
raregodb <ggentelmann@yandex.ru>
2025-05-14 01:27:58 +0300

    feat: header init. refs #3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthorisationPage from "@/routes/reg-auth-page/AuthorisationPage.tsx";
+import Header from "@/components/custom/header/Header.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
+          <Header/>
           <Routes>
               <Route path={"/"} element={<App/>} />
               <Route path={"/auth"} element={<AuthorisationPage/>} />
