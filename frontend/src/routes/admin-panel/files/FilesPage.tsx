7e415e3ecbe8a8a58ab0183e5dda758b56199989
raregodb <ggentelmann@yandex.ru>
2025-05-21 14:36:55 +0300

    feat: file table and faker. refs #61

--- frontend/src/routes/admin-panel/files/FilesPage.tsx
+++ frontend/src/routes/admin-panel/files/FilesPage.tsx
@@ -1,8 +1,20 @@
+import FilesAdminTableView, {generateMockFiles} from "@/routes/admin-panel/files/components/FilesAdminTableView.tsx";
+import {Label} from "@/components/ui/label.tsx";
+import {BatchLoader} from "@/components/custom/BatchLoader/BatchLoader.tsx";
+
 function FilesPage() {
+    const data = generateMockFiles(30);
+
     return (
-        <div>
-            Ну Файлы
-        </div>
+        <BatchLoader states={[]}
+                     loadingMessage={"Загрузка файлов"}
+                     display={() =>
+                         <div className={"flex flex-col p-8"}>
+                             <Label className={"text-4xl"}>Файлы</Label>
+                             <FilesAdminTableView data={data}></FilesAdminTableView>
+                         </div>
+                     }
+        />
     )
 }
 
---
+++ frontend/src/routes/admin-panel/files/components/FilesAdminTableView.tsx
@@ -0,0 +1,61 @@
+
+import RichTableView from "@/components/custom/table/RichTableView.tsx";
+import {columnsFiles} from "@/columns/columnsFiles.tsx";
+import { faker } from "@faker-js/faker";
+
+export interface tempFile {
+    hash: string,
+    name: string,
+    size: string,
+    countStrings: number,
+    createdAt: string,
+    type: string,
+    commitName: string,
+    repoName: string,
+    author: string,
+    changedAuthor: string
+}
+
+export function generateMockFiles(count = 20): tempFile[] {
+    const types = ['FILE', 'DIRECTORY'];
+
+    return Array.from({ length: count }, (_, index) => ({
+        hash: faker.string.alphanumeric(40),
+        name: faker.system.fileName(),
+        size: `${faker.number.int({ min: 100, max: 10_000 })} KB`,
+        countStrings: faker.number.int({ min: 10, max: 1000 }),
+        createdAt: faker.date.recent().toISOString(),
+        type: faker.helpers.arrayElement(types),
+        commitName: `commit-${faker.git.commitSha().slice(0, 7)}`,
+        repoName: "repo",
+        author: faker.internet.userName(),
+        changedAuthor: faker.internet.userName(),
+    }));
+}
+
+function FilesAdminTableView(props: {
+    data: tempFile[]
+}) {
+    return (
+        <>
+            <>
+                <RichTableView
+                    entries={props.data}
+                    tableConfig={{
+                        columns: columnsFiles
+                    }}
+                    settings={{
+                        enableSearch: true,
+                        enableVisualization: true,
+                        enableColumnVisibilityToggle: true,
+                        rowClickHandler: (user) => {
+                            // navigate(`/spaces/${selectedSpaceId}/dashboard/users/${user._id}`)
+                        }
+                    }}
+                />
+            </>
+        </>
+    )
+}
+
+export default FilesAdminTableView
